dbbc90201655a1159e9237b2558cd1792f5da350
Ashmil Abdul Rasheed <ashmilabdulrasheed@gmail.com>
2022-11-16 12:59:31 +0530

    serializers created

--- books/models.py
+++ books/models.py
@@ -7,3 +7,6 @@ from .choices import StatusChoice
 class Books(models.Model):
     bookName = models.CharField(max_length=200, null=False, help_text='This field is required')
     status = models.CharField(max_length=100, choices=StatusChoice.choices, default=StatusChoice.TO_READ)
+
+    def __str__(self):
+        return self.bookName
