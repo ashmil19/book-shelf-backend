04b5e3952322e45a25b527383527455d73f30083
Ashmil Abdul Rasheed <ashmilabdulrasheed@gmail.com>
2022-11-16 13:25:02 +0530

    view functions created

--- books/views.py
+++ books/views.py
@@ -1,7 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Books
+from .serializers import *
+
 
 # Create your views here.
 
-def view(request):
-    return HttpResponse("hello")
+@api_view(['GET'])
+def get_all_books(request):
+    books = Books.objects.all()
+    serializer = BooksSerializer(books, many=True)
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_book(request, pk):
+    book = Books.objects.get(id=pk)
+    serializer = BooksSerializer(book, many=False)
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def add_book(request):
+    serializer = BooksSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def delete_book(request, pk):
+    book = Books.objects.get(id=pk)
+    book.delete()
+
+    return Response('Book successfully deleted')
